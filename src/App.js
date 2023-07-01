03e504c4a610816bcc716179cdb77eb4004a32b9
almacode1703 <testmygitt.2023@gmail.com>
2023-07-02 00:12:28 +0530

    Compass Clock

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Compass from './components/Compass';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Compass Clock</h1>
+      <Compass/>
+    </div>
+  );
+}
+
+export default App;
